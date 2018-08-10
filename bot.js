7c504e2701df07a1c42911a3f15c5439cdc86d01
F166rb <41041972+F166rb@users.noreply.github.com>
2018-08-10 05:49:12 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,14 +1,31 @@
 const Discord = require('discord.js');
+const ytdl = require("ytdl-core");
+const { Client, Util } = require('discord.js');
+const getYoutubeID = require('get-youtube-id');
+const fetchVideoInfo = require('youtube-info');
+const YouTube = require('simple-youtube-api');
+const youtube = new YouTube("AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8");
+const queue = new Map();
 const client = new Discord.Client();
-const prefix = '!'
+const prefix = '-'
 
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+    console.log('----------------');
+    console.log(`✨ Id: Nizx`);
+    console.log(`✨ Prefix: Soon`);
+    console.log(`✨ Servers: 1`);
+    console.log(`✨ Members: 1000`);
+    console.log(`✨ Channels: Limtit`);
+    console.log('----------------');
+    console.log(`Logged in as `);
+    client.user.setGame(`⠀⠀⠀⠀⠀⠀SPT Server System Bot.   `, "http://twitch.tv/Streammingg")
+    client.user.setStatus("dnd")
 });
+
 
 client.on('message', msg => {
   if (msg.content === 'شغال؟bc') {
-    msg.reply('ايه انا كفو.');
+    msg.reply('ايه انا قوي.');
   }
 });
 
